140c3578592f864187a06004439ac219c6a672c8
garbuzov <k.garbuzov@cft.ru>
2019-04-19 19:43:04 +0700

    Added basic test for ListInverter

---
+++ app/src/test/java/ru/ftc/focusstart/unittests/ListInverterTest.java
@@ -0,0 +1,35 @@
+package ru.ftc.focusstart.unittests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ListInverterTest {
+
+    private ListInverter listInverter;
+
+    @Before
+    public void setUp() {
+        listInverter = new ListInverter();
+    }
+
+    @Test
+    public void testInvertNormalList() {
+        List<String> input = new ArrayList<>();
+        input.add("qwe");
+        input.add("asd");
+        input.add("zxc");
+
+        List<String> actualResult = listInverter.invertList(input);
+
+        List<String> expectedResult = new ArrayList<>();
+        expectedResult.add("zxc");
+        expectedResult.add("asd");
+        expectedResult.add("qwe");
+        assertEquals(expectedResult, actualResult);
+    }
+}
